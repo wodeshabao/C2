18caf5bcb22dfc757a80050e36acc5621e1be432
第一次 <123456456@qq.com>
2019-01-15 14:27:03 +0800

    完成基本框架

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("敏感字符管理系统\n");
+
+    while (1)
+    {
+        printf("1---添加敏感字符\n");
+        printf("2---删除敏感字符（最后一个）\n");
+        printf("3---查看敏感字符\n");
+        printf("4---替换敏感字符\n");
+        printf("5---退出\n");
+
+        printf("请输入相应的功能编号\n");
+
+        int code;
+        scanf("%d",&code);
+        if(code==1){
+
+        }
+        if(code==2){
+            
+        }
+        if(code==3){
+            
+        }
+        if(code==4){
+            
+        }
+        if(code==5){
+            
+        }
+    }
 
     return 0;
-
 }
