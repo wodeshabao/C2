1306341d76191ed0b48ccd96691958f3f28f5a6f
第一次 <123456456@qq.com>
2019-01-15 14:51:27 +0800

    添加敏感字符功能

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+char arr[1024];
+int index=0;
 
 int main()
 {
@@ -16,21 +18,34 @@ int main()
         printf("请输入相应的功能编号\n");
 
         int code;
-        scanf("%d",&code);
-        if(code==1){
+        scanf("%d", &code);
 
+        if (code == 1)
+        {
+            printf("请输入要添加的敏感字符\n");
+            char m;
+            scanf("%d",&m);
+            scanf("%d",&m);
+
+            arr[index]=m;
+            index++;
+
+            printf("添加成功，点击回车继续\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
-        if(code==2){
-            
+        if (code == 2)
+        {
         }
-        if(code==3){
-            
+        if (code == 3)
+        {
         }
-        if(code==4){
-            
+        if (code == 4)
+        {
         }
-        if(code==5){
-            
+        if (code == 5)
+        {
         }
     }
 
